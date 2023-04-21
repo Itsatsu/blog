b8fd8c18d5cd72e8b8218ef6e51c461295e3fc1a
Eric G <e.gigondan@gmail.com>
2023-04-21 11:59:39 +0200

    update controller

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -6,12 +6,12 @@ use Twig\Loader\FilesystemLoader;
 
 class Controller
 {
-    public function view(string $path)
+    public function view(string $path,array $vars = [])
     {
         $loader = new FilesystemLoader('./src/templates/');
         $twig = new Environment($loader);
 
-        echo $twig->render($path);
+        echo $twig->render($path, $vars);
     }
 
 }
